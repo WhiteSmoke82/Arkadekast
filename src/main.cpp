fdf10854625e54bb44a53ef1ac7624acb4ae50ff
WhiteSmoke <31653561+WhiteSmoke82@users.noreply.github.com>
2025-04-02 21:32:47 +0200

    Initial push

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int dirPin = 19;
+const int stepPin = 18;
+const int stepsPerRevolution = 200;
+const int ms1Pin = 27;
+const int ms2Pin = 26;
+const int ms3Pin = 25;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup(){
+	pinMode(stepPin, OUTPUT);
+	pinMode(dirPin, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop(){
+	digitalWrite(dirPin, HIGH);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+	for(int x = 0; x < stepsPerRevolution; x++)
+	{
+		digitalWrite(stepPin, HIGH);
+		delayMicroseconds(2000);
+		digitalWrite(stepPin, LOW);
+		delayMicroseconds(2000);
+	}
+	delay(1000);
+	
+	digitalWrite(dirPin, LOW);
+
+	for(int x = 0; x < stepsPerRevolution; x++)
+	{
+		digitalWrite(stepPin, HIGH);
+		delayMicroseconds(1000);
+		digitalWrite(stepPin, LOW);
+		delayMicroseconds(1000);
+	}
+	delay(1000);
 }

d5a054a9414623ab59637821c49df2a204c2f86a
WhiteSmoke <31653561+WhiteSmoke82@users.noreply.github.com>
2025-04-02 22:48:44 +0200

    Last update

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ const int ms1Pin = 27;
 const int ms2Pin = 26;
 const int ms3Pin = 25;
 
-int speedDelay = 250;
+int speedDelay = 200;
 
 void setup(){
 	pinMode(stepPin, OUTPUT);
@@ -15,9 +15,11 @@ void setup(){
   pinMode(ms1Pin, OUTPUT);
   pinMode(ms2Pin, OUTPUT);
   pinMode(ms3Pin, OUTPUT);
+
   digitalWrite(ms1Pin, HIGH);
   digitalWrite(ms1Pin, HIGH);
   digitalWrite(ms1Pin, HIGH);
+  
   digitalWrite(dirPin, HIGH);
 }
 
